f86428aecf9f768fd156f38e1a62e7d0b23cf6f4
fezfezz <52402976+fezfezz@users.noreply.github.com>
2019-07-02 21:09:23 +0800

    Fully working Password validation
    TO DO: -Increase the blacklist on password validation 
                 -Maybe make a blacklist for username (?)
                 -Put Password and Usernames VERIFICATIONS into own def functions

---
+++ UsernamePasswordIO.py
@@ -0,0 +1,56 @@
+
+usernameList = []
+passwordList = []
+passwordBlackList = [",", "_", "[", "]", ";", "'", '"',"/", "=", "+", "-", ".", "!", "@"]
+
+# Validates that all characters in user inputted Password do not match the black list
+def validatePassword(inputPass):
+    passwordBlackList = [",", "_", "[", "]", ";", "'", '"', "/", "=", "+", "-", ".", "!", "@"]
+    if any(char in passwordBlackList for char in inputPass):
+        return False
+    return True
+
+# Creates a new Username
+def NewUser():
+    newUser = (input("Please enter a new username: "))
+    usernameList.append(newUser)
+
+# Creates a new Password
+def NewPass():
+    passMessage = "Please enter a new password: "
+    inputPass = input(passMessage)
+    while validatePassword(inputPass) == False:
+        passMessage = "ERROR: Please do not include illegal characters (, . _ etc)"
+        inputPass = input(passMessage)
+
+    else:
+        newPass = inputPass
+        passwordList.append(newPass)
+
+# Creation
+NewUser()
+NewPass()
+
+# Username Verification
+inputUser = ""
+userMessage = "Please enter your username: "
+
+while inputUser not in usernameList:
+    inputUser = input(userMessage)
+    userMessage = "ERROR: Username cannot be found, Please re-try: "
+
+if inputUser in usernameList:
+    userNumber = usernameList.index(inputUser)
+    print("Username found!")
+
+# Password Verification
+correctPassword = passwordList[userNumber]
+inputPassword = ""
+passMessage = "Please enter your password: "
+
+while correctPassword != inputPassword:
+    inputPassword = (input(passMessage))
+    passMessage = "ERROR: Password is incorrect!, Please re-try: "
+
+if correctPassword == inputPassword:
+    print("Welcome, %s!" % usernameList[userNumber])
