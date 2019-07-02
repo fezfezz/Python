2cb11e6ef00dffeb78a145e552e0ec7491f966b2
fezfezz <52402976+fezfezz@users.noreply.github.com>
2019-07-02 18:02:48 +0800

    PasswordIO

--- testing stuff.py
+++ testing stuff.py
@@ -1,23 +1,37 @@
 
 usernameList = []
-passwordList = ["rikimaru"]
-
+passwordList = []
+passwordBlackList = [",", "_", "[", "]", ";", "'", '"',"/", "=", "+", "-", ".", "!", "@"]
 
 def NewUser():
     newUser = (input("Please enter a new username: "))
     usernameList.append(newUser)
 
+def NewPass():
+    passMessage = "Please enter a new password: "
+    newPass = "."
+    for x in passwordBlackList:
+            for y in newPass:
+                if x in y:
+                    newPass = (input(passMessage))
+                    passMessage = "ERROR: Please do not include illegal characters (, . _ etc)"
+    passwordList.append(newPass)
+
 # Creation
 NewUser()
+NewPass()
 
 # Username
 inputUser = ""
 userMessage = "Please enter your username: "
+
 while inputUser not in usernameList:
     inputUser = input(userMessage)
     userMessage = "ERROR: Username cannot be found, Please re-try: "
+
 if inputUser in usernameList:
     userNumber = usernameList.index(inputUser)
+
 
 # Password
 correctPassword = passwordList[userNumber]
